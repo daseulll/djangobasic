95942f00a5aaa3a1e1529b0ec1940ae4547d775e
daseulll <nldaseul@gmail.com>
2019-03-04 10:17:44 +0900

    set postgresql_db for prod_heroku.py

--- djangobasic/settings/prod_heroku.py
+++ djangobasic/settings/prod_heroku.py
@@ -2,10 +2,10 @@ from .common import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangobasic_db',   
-        'USER' : 'root',            
-        'PASSWORD' : 'qwer1234',    
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',   
+        'USER' : 'postgres',            
+        'PASSWORD' : 'postgres1234',    
         'HOST' : '127.0.0.1',
         'OPTIONS' : {},
     }
